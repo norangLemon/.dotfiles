577c1c107e7ecf8d8feb6ff65e745e2308264142
norang@lemon <pinethee@naver.com>
2015-12-27 20:34:51 +0000

    install.sh에 vundle install 명령행 추가

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PlugUpdate +qall
