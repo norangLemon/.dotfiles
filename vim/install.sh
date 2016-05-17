b0b3026d519d633333f94119c1d270e7b7f1d602
norang <pinethee@naver.com>
2016-05-17 15:31:00 +0900

    vim 디렉토리 해제

--- vim/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugUpdate +qall
--- vim/update.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-vim +PlugUpgrade +PlugClean +PlugUpdate +qall
