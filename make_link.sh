d2acb8735dc7bc2594989214616385cfab181048
norang <noranglemon@gmail.com>
2017-11-13 10:27:00 +0900

    add gitexclude simlink

--- make_link.sh
+++ make_link.sh
@@ -3,6 +3,7 @@ ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/gitexclude ~/.gitexclude
 ln -sf ~/.dotfiles/config ~/.ssh/config
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/editorconfig ~/.editorconfig
