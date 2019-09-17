f3cdf30c3f4d13ea41aabdce6fef7197ecbbd920
norang <noranglemon@gmail.com>
2019-09-17 15:52:40 +0900

    Modify backup function
    Change-Id: I287a4e54753bf43f051cac539fd20504eda768f8

--- make_link.sh
+++ make_link.sh
@@ -3,17 +3,29 @@
 # initialize CORP using hostname.sh
 if [[ -e "exclusives/hostname.sh" ]]; then
     source "exclusives/hostname.sh"
+    CORP="$HOSTNAME_ALIAS"
 fi
 
 # backup function to move existing config file
 function backup {
     FILE="$HOME/$1"
-    if [ -L "$FILE" ]; then
-        rm "$FILE"
-        echo -e "$FILE is symlink; \e[1mremoved\e[0m"
-    elif [ -e "$FILE" ]; then
-        mv "$FILE" "$FILE.backup"
-        echo -e "$FILE \e[1mis moved to\e[0m $FILE.backup"
+    if [ ! -e "$FILE" ]; then
+        return
+    fi
+    DEST="$FILE"
+    NUM=0
+    while [ -e "$DEST" ]; do
+        if [ -L "$DEST" ]; then
+            rm "$DEST"
+            echo -e "$DEST is symlink; \e[1mremoved\e[0m"
+            break
+        fi
+        ((NUM++))
+        DEST="$FILE.backup$NUM"
+    done
+    if [ -e "$FILE" ]; then
+        mv "$FILE" "$DEST"
+        echo -e "$FILE \e[1mis moved to\e[0m $DEST"
     fi
 }
 
@@ -33,6 +45,11 @@ if [[ -e "exclusives/vimrc" && -n "$CORP" ]]; then
     echo -e "  \e[1mUse exclusive vimrc\e[0m"
 else
     ln -s ~/.dotfiles/vimrc ~/.vimrc
+fi
+
+backup ".hgrc"
+if [[ -e "exclusives/hgrc" && -n "$CORP" ]]; then
+    ln -s ~/.dotfiles/exclusives/hgrc ~/.hgrc
 fi
 
 backup ".bash_profile"
